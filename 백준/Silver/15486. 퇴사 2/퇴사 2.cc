9c86a4c82b687b050d0ff83ba182fb6f0b749b99
Seunghyun Kim <63703962+shyuuuuni@users.noreply.github.com>
2022-03-31 07:39:02 +0900

    [Silver I] Title: 퇴사 2, Time: 232 ms, Memory: 37116 KB -BaekjoonHub

---
+++ 백준/Silver/15486. 퇴사 2/퇴사 2.cc
@@ -0,0 +1,88 @@
+/*
+Time : 2022.02.14 (10 mins)
+Problem : BOJ15486 (https://www.acmicpc.net/problem/15486)
+Algorithm Type : DP
+Outline :
+N일동안 하루에 하나씩 상담 요청이 들어온다.
+상담은 걸리는 시간 T와 받을 수 있는 비용 P로 구분되어 들어온다.
+만약 상담 중 N일이 지나면 해당 상담은 받을 수 없다.
+상담을 적절히 했을 때, 받을 수 있는 최대 비용을 계산한다.
+Method :
+dp[i]를 날짜를 역순으로 봤을 때, 현재(i번째) 까지 얻을 수 있는 최대 이득이라고 생각한다.
+이때 dp[i]는 다음과 같은 두 가지 경우가 있다.
+1. i번째 날짜의 상담을 진행하는 경우 -> i부터 t일간 상담을 하고 p의 이득 + dp[i+t]
+2. i번째 날짜의 상담을 진행하지 않는 경우 -> 이전까지의 최대 이득과 동일
+따라서 dp배열의 범위가 n을 넘지 않도록 하면서 해당 조건의 점화식을 따르면 해결할 수 있다.
+Example :
+7
+3 10
+5 20
+1 10
+1 20
+2 15
+4 40
+2 200
+-------
+45
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define pii pair<int,int>
+#define mp(X,Y) make_pair((X),(Y))
+typedef long long ll;
+
+bool compare(const string &a, const string &b) {
+  // 왼쪽 변수 a, 오른쪽 변수 b에 대해서
+  // a<b 라면 오른쪽이 더 크도록 정렬, a>b 라면 왼쪽이 더 크도록 정렬
+  // a가 b보다 앞에 와야 한다면 true, 아니면 false (같을때는 꼭 false 반환하도록 해야 함)
+  // 래퍼런스 사용하는게 좋음
+  return true;
+}
+
+// 셋 정렬용 구조체
+struct setOrder {
+  bool operator() (const string& left, const string& right) const {
+    // 비교함수
+    return true;
+  }
+};
+
+// 문자열을 구분자를 기준으로 나눠서 벡터에 저장
+int splitStringByToken(vector<string> &v, string &s, const char &del) {
+  if(s.empty()) return 0;
+  istringstream ss(s);
+  string token;
+
+  v.clear();
+  while (getline(ss, token, del)) {
+    if(!token.empty()) v.push_back(token);
+  }
+
+  return v.size();
+}
+
+int main(void) {
+  ios::sync_with_stdio(0); cin.tie(0);
+  int n;
+  cin >> n;
+  pair<ll,ll> arr[1500000+5] = {};
+  ll dp[1500000+5] = {};
+  for(int i=0; i<n; i++) {
+    ll t, p;
+    cin >> t >> p;
+    arr[i] = mp(t,p);
+  }
+  for(int i=n-1; 0<=i; i--) {
+    ll t = arr[i].first;
+    ll p = arr[i].second;
+    if(n < i+t) {
+      dp[i] = dp[i+1];
+      continue;
+    }
+    dp[i] = max(dp[i+1], p + dp[i+t]);
+  }
+  cout << *max_element(dp, dp+n);
+  return 0;
+}
