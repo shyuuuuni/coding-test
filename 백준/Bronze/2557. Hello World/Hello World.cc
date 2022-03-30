c0943b3e7728aff0732499fc5decf1821a1c45bf
Seunghyun Kim <63703962+shyuuuuni@users.noreply.github.com>
2022-03-31 06:55:48 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -0,0 +1,16 @@
+/*
+Time : 2022.01.06 (1 min)
+Problem : BOJ2557 (https://www.acmicpc.net/problem/2557)
+Algorithm Type : I/O
+Outline :
+Hello World! 를 출력한다.
+Method :
+cout 함수를 이용하여 문자열을 출력한다.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main(void) {
+  ios::sync_with_stdio(0); cin.tie(0);
+  cout << "Hello World!";
+  return 0;
+}
