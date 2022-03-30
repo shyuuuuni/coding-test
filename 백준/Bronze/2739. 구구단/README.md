9ab8bf36784c6bb4e907d30091c5374a48ca5046
Seunghyun Kim <63703962+shyuuuuni@users.noreply.github.com>
2022-03-31 06:59:29 +0900

    [Bronze III] Title: 구구단, Time: 0 ms, Memory: 2080 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/README.md
@@ -0,0 +1,12 @@
+# [Bronze III] 구구단 - 2739 
+
+[문제 링크](https://www.acmicpc.net/problem/2739) 
+
+### 성능 요약
+
+메모리: 2080 KB, 시간: 0 ms
+
+### 분류
+
+사칙연산(arithmetic), 구현(implementation), 수학(math)
+
---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,68 @@
+/*
+Time : 2022.02.12 (10 mins)
+Problem : BOJ2739 (https://www.acmicpc.net/problem/2739)
+Algorithm Type : IO
+Outline :
+정수 N이 주어지면 N에 대한 구구단을 출력한다.
+Method :
+반복문을 통해 출력한다.
+Example :
+2
+-------
+2 * 1 = 2
+2 * 2 = 4
+2 * 3 = 6
+2 * 4 = 8
+2 * 5 = 10
+2 * 6 = 12
+2 * 7 = 14
+2 * 8 = 16
+2 * 9 = 18
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define pii pair<int,int>
+#define mp(X,Y) make_pair((X),(Y))
+typedef long long ll;
+
+bool compare(const string &a, const string &b) {
+  // 왼쪽 변수 a, 오른쪽 변수 b에 대해서
+  // a<b 라면 오른쪽이 더 크도록 정렬, a>b 라면 왼쪽이 더 크도록 정렬
+  // a가 b보다 앞에 와야 한다면 true, 아니면 false (같을때는 꼭 false 반환하도록 해야 함)
+  // 래퍼런스 사용하는게 좋음
+  return true;
+}
+
+// 셋 정렬용 구조체
+struct setOrder {
+  bool operator() (const string& left, const string& right) const {
+    // 비교함수
+    return true;
+  }
+};
+
+// 문자열을 구분자를 기준으로 나눠서 벡터에 저장
+int splitStringByToken(vector<string> &v, string &s, const char &del) {
+  if(s.empty()) return 0;
+  istringstream ss(s);
+  string token;
+
+  v.clear();
+  while (getline(ss, token, del)) {
+    if(!token.empty()) v.push_back(token);
+  }
+
+  return v.size();
+}
+
+int main(void) {
+  ios::sync_with_stdio(0); cin.tie(0);
+  int n;
+  cin >> n;
+  for(int i=1; i<=9; i++) {
+    cout << n << " * " << i << " = " << n*i << "\n";
+  }
+  return 0;
+}
