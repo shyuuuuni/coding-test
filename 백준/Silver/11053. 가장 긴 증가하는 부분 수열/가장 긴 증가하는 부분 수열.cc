03d3cf340a6af5ec6a584d4c2f81d79d86c905f2
Seunghyun Kim <63703962+shyuuuuni@users.noreply.github.com>
2022-03-31 07:22:30 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,59 @@
+/*
+Time : 2022.02.10 (10 mins)
+Problem : BOJ11053 (https://www.acmicpc.net/problem/11053)
+Algorithm Type : Dynamic Programming
+Outline :
+수열 A가 주어졌을 때, 그 수열의 증가 부분 수열 중에서 합이 가장 긴 부분수열의 길이를 구하시오.
+Method :
+DP를 이용한 O(N^2)풀이로 해결하였다.
+dp[i]를 i번째 원소 arr[i]를 마지막으로 하는 부분수열의 최대 길이로 정의한다.
+0 <= j < i 인 j에 대해서, arr[j] < arr[i]인 j가 존재한다면
+arr[j], ..., arr[i] 로 하는 부분수열이 존재하므로 갱신한다.
+따라서 위 범위에서 dp[i] = max(dp[i], dp[j]+1) 점화식으로 갱신하면
+모든 i에 대해서 부분수열의 최대 길이를 구할 수 있다.
+Example :
+6
+10 20 10 30 20 50
+-------
+4
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define pii pair<int,int>
+#define mp(X,Y) make_pair((X),(Y))
+typedef long long ll;
+
+bool compare(const string &a, const string &b) {
+  // 왼쪽 변수 a, 오른쪽 변수 b에 대해서
+  // a<b 라면 오른쪽이 더 크도록 정렬, a>b 라면 왼쪽이 더 크도록 정렬
+  // a가 b보다 앞에 와야 한다면 true, 아니면 false (같을때는 꼭 false 반환하도록 해야 함)
+  // 래퍼런스 사용하는게 좋음
+  return true;
+}
+
+// 셋 정렬용 구조체
+struct setOrder {
+  bool operator() (const string& left, const string& right) const {
+    // 비교함수
+    return true;
+  }
+};
+
+int main(void) {
+  ios::sync_with_stdio(0); cin.tie(0);
+  int n;
+  cin >> n;
+  int arr[1000+5] = {};
+  int dp[1000+5] = {}; // dp[i] : i번째 원소를 포함한 증가하는 부분수열의 최대 합
+  for(int i=0; i<n; i++) cin >> arr[i];
+  for(int i=0; i<n; i++) {
+    dp[i] = 1;
+    for(int j=0; j<i; j++) {
+      if(arr[j] < arr[i]) dp[i] = max(dp[i], dp[j]+1);
+    }
+  }
+  cout << *max_element(dp,dp+n);
+  return 0;
+}
