28d215182b78d0cbe9b6bfe5cffc51de96f00108
Seung-hyun Kim <63703962+shyuuuuni@users.noreply.github.com>
2024-01-16 00:37:58 +0900

    [level Lv.3] Title: 숫자 게임, Time: 90.50 ms, Memory: 45.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/12987. 숫자 게임/숫자 게임.js
@@ -0,0 +1,22 @@
+function solution(A, B) {
+    let answer = 0;
+    
+    const sortedA = A.sort((a,b) => -(a-b)),
+          sortedB = B.sort((a,b) => -(a-b));
+    
+    let ptrA = 0,
+        ptrB = 0;
+    
+    while (ptrA < sortedA.length && ptrB < sortedB.length) {
+        if (sortedA[ptrA] < sortedB[ptrB]) {
+            answer++;
+            ptrA++;
+            ptrB++;
+        } else {
+            ptrA++;
+        }
+    }
+    
+    
+    return answer;
+}
